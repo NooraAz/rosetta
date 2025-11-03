d54768996cc6e771230f1e3782555d63c72b3b61
Noora Azadvari <eazadvar@uoregon.edu>
2025-11-03 15:35:32 -0800

    adding the ability to score the pose and print it out

--- source/src/apps/pilot/nooraaz/bootcamp.cc
+++ source/src/apps/pilot/nooraaz/bootcamp.cc
@@ -17,6 +17,11 @@
 #include <basic/options/option.hh>
 #include <basic/options/keys/in.OptionKeys.gen.hh>
 #include <devel/init.hh>
+#include <utility/pointer/owning_ptr.hh>
+#include <core/pose/Pose.hh>
+#include <core/import_pose/import_pose.hh>
+#include <core/scoring/ScoreFunction.hh>
+#include <core/scoring/ScoreFunctionFactory.hh>
 
 int main( int argc, char ** argv ) {
   std::cout << "Hello World!" << std::endl;
@@ -24,6 +29,10 @@ int main( int argc, char ** argv ) {
   utility::vector1< std::string > filenames = basic::options::option[ basic::options::OptionKeys::in::file::s ].value();
   if ( filenames.size() > 0 ) {
     std::cout << "You entered: " << filenames[ 1 ] << " as the PDB file to be read" << std::endl;
+    core::pose::PoseOP mypose = core::import_pose::pose_from_file( filenames[1] );
+    core::scoring::ScoreFunctionOP sfxn = core::scoring::get_score_function();
+    core::Real score = sfxn->score( *mypose );
+    std::cout << "The score is: " << score << std::endl;
   } else {
     std::cout << "You didn’t provide a PDB file with the -in::file::s option" << std::endl;
     return 1;
